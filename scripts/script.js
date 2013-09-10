8bda014f5e0a279445c34ddd9ad902b614d0ffa9
Gias Kay Lee <balancetraveller+github@gmail.com>
2013-09-10 18:20:01 +0800

    Step 1: Writing Data to Your Firebase - `Firebase.set()` and `Firebase.update()`

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,24 @@
 'use strict';
 
 (function() {
+    var Data = new Firebase('https://packt-firebase-demo.firebaseio.com/');
+
+    Data.set({
+        name: 'Sandy',
+        gender: 'f'
+    });
+
+    Data.update({
+        name: 'Karen'
+    });
+
+    /*/
+    / / The result will look like the following:
+    / / 
+    / / {
+    / /     "name": "Karen",
+    / /
+    / /     "gender": "f"
+    / / }
+    /*/
 })();
