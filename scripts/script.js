127b78aedad97e313b9a60118877c3399e431fd5
Gias Kay Lee <balancetraveller+github@gmail.com>
2014-04-14 14:32:25 +0800

    [WIP]

--- scripts/script.js
+++ scripts/script.js
@@ -27,13 +27,39 @@
         gender: 'm'
     });
 
-    /*/
-    / / The result will look like the following:
-    / / 
-    / / {
-    / /     "name": "Squidward",
-    / /
-    / /     "gender": "m"
-    / / }
-    /*/
+    Data.on('value', function(data) {
+        console.log('Value: ' + JSON.stringify(data.val()));
+
+        /*/
+        / / The console logs will look like the following:
+        / /
+        / / Value: {"gender":"m","name":"Squidward"}
+        / /
+        / / Value: {"gender":"f","name":"Sandy"}
+        / /
+        / / Value: {"gender":"f","name":"Karen"}
+        / /
+        / / Value: {"-J36HiIlZjs1DZm1o95u":{"gender":"m","name":"Patrick"},"gender":"f","name":"Karen"}
+        / /
+        / / Value: {"-J36HiIlZjs1DZm1o95u":{"gender":"m","name":"Patrick"},"-J36HiIm57U0P5hqVZGW":{"gender":"m","name":"Plankton"},"gender":"f","name":"Karen"}
+        / /
+        / / Value: {"gender":"m","name":"Squidward"}
+        /*/
+    });
+
+    Data.on('child_added', function(data) {
+        console.log('Child Added: ' + JSON.stringify(data.val()));
+
+        /*/
+        / / The console logs will look like the following:
+        / /
+        / / Child Added: "f"
+        / /
+        / / Child Added: "Sandy"
+        / /
+        / / Child Added: {"gender":"m","name":"Patrick"}
+        / /
+        / / Child Added: {"gender":"m","name":"Plankton"}
+        /*/
+    });
 })();
